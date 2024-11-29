4b9f7004da39a4bf2d5888f1ce391a202ce27f1b
Félix Hernández <felixhernandezmy@gmail.com>
2024-11-29 19:57:10 +0100

    Update Message.cpp

--- Motor/code/sources/Message.cpp
+++ Motor/code/sources/Message.cpp
@@ -1,20 +1,22 @@
+// Autor: felixhmy
+
 #include "Message.hpp"
 
 namespace engine
 {
-    // Añade un listener para un tipo especifico de mensaje.
+    // AÃ±ade un listener para un tipo especifico de mensaje.
     void Message_Dispatcher::add(Message_Listener& listener, const std::string& message_id)
     {
         message_listeners[message_id].push_back(&listener);
     }
 
-    // Añade un mensaje a la lista de mensajes por enviar.
+    // AÃ±ade un mensaje a la lista de mensajes por enviar.
     void Message_Dispatcher::send(const Message& message)
     {
         messages.push_back(message);
     }
 
-    // Envía todos los mensajes pendientes a sus listerners.
+    // EnvÃ­a todos los mensajes pendientes a sus listerners.
     void Message_Dispatcher::execute(float t)
     {
         for (auto& message : messages)
